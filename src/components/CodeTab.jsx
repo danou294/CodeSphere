1c1735a7ba2b6f518549e377f2452fd673bf855e
Daniel <danielevy29@gmail.com>
2024-02-25 01:48:16 +0100

    monaco-edite+autosuggest

--- src/components/CodeTab.jsx
+++ src/components/CodeTab.jsx
@@ -1,15 +1,37 @@
+import React from 'react';
 import { useDispatch } from "react-redux";
 import { updateCode } from "../features/tabs";
+import Editor from '@monaco-editor/react';
 
-export default function CodeTab({ code, id }) {
-  const dispatch = useDispatch();
+export default function CodeTab({ code, id, lang }) {
+    const dispatch = useDispatch();
 
-  return (
-      <textarea
-          onChange={e => dispatch(updateCode({ id, value: e.target.value }))}
-          value={code}
-          spellCheck="false"
-          className="bg-gray-900 text-white text-lg p-4 block w-full h-full focus:outline-none resize-none border-none"
-      ></textarea>
-  );
+    function handleEditorChange(value) {
+        // Mise à jour de l'état Redux avec le nouveau code
+        dispatch(updateCode({ id, value }));
+    }
+
+    // Sélectionnez le langage par défaut de l'éditeur en fonction de l'onglet
+    const languageMap = {
+        html: "html",
+        css: "css",
+        javascript: "javascript",
+    };
+    const defaultLanguage = languageMap[lang];
+
+    return (
+        <Editor
+            value={code}
+            language={defaultLanguage}
+            theme="vs-dark"
+            options={{
+                fontSize: 14,
+                minimap: { enabled: true },
+                contextmenu: false,
+                automaticLayout: true, // Ajuste automatiquement la taille de l'éditeur
+            }}
+            onChange={handleEditorChange}
+            className="h-full w-full"
+        />
+    );
 }
