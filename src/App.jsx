094014bdf324d5b4f2274676a8d3e97c8ecb437e
Daniel LEVY <d.levy@socoda.com>
2024-03-12 15:26:43 +0100

    refonte

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,10 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { AuthProvider } from './components/Contexts/AuthContext.jsx';
 import Navbar from './components/Navbar'; // Assurez-vous que le chemin est correct
 import Tabs from "./components/IDE/Tabs.jsx";
-import PreviewButton from "./components/IDE/PreviewButton.jsx";
 import SignupForm from './components/Auth/SignupForm';
 import LoginForm from './components/Auth/LoginForm';
 import ForgotPasswordForm from "./components/Auth/ForgotPasswordForm.jsx";
+import Home from './components/Home';
 
 function App() {
     return (
@@ -14,16 +14,12 @@ function App() {
             <Router>
                 <Navbar /> {/* La Navbar est ajoutée ici */}
                 <div className="bg-gray-800 min-h-screen flex flex-col text-gray-200">
-                    <div className="px-6 py-5 flex justify-between items-center border-b border-gray-700">
-                        {/* Si PreviewButton doit rester dans cette zone, assurez-vous qu'il s'aligne bien avec votre UI */}
-                        <PreviewButton />
-                    </div>
                     {/* Assurez-vous que Tabs est correctement intégré dans votre layout */}
                     <Routes>
                         <Route path="/signup" element={<SignupForm />} />
                         <Route path="/login" element={<LoginForm />} />
-                        {/* La route principale pour Tabs */}
-                        <Route path="/" element={<Tabs />} />
+                        <Route path="/" element={<Home />} />
+                        <Route path="/editor" element={<Tabs />} />
                         <Route path="/forgot-password" element={<ForgotPasswordForm />} />
                     </Routes>
                 </div>
--- src/components/IDE/ButtonTab.jsx
+++ src/components/IDE/ButtonTab.jsx
@@ -1,8 +1,11 @@
+// ButtonTab.jsx
+import React from 'react';
 import { useDispatch } from "react-redux";
 import { hidePreview } from "../../features/preview.js";
 
 export default function ButtonTab({ id, toggleTab, buttonContent, imgURL, isActive }) {
     const dispatch = useDispatch();
+    const hoverStyle = "hover:bg-blue-700 hover:text-white"; // Ajout du style de hover
 
     return (
         <button
@@ -10,11 +13,10 @@ export default function ButtonTab({ id, toggleTab, buttonContent, imgURL, isActi
                 toggleTab(id);
                 dispatch(hidePreview());
             }}
-            className={`flex items-center px-4 py-2 rounded transition duration-150 ease-in-out ${isActive ? 'bg-blue-500' : 'hover:bg-gray-700'} `}
+            className={`flex items-center px-4 py-2 rounded transition duration-150 ease-in-out ${isActive ? 'bg-blue-500 text-white' : 'text-gray-700'} ${hoverStyle}`}
         >
-            <img src={imgURL} className="w-6 h-6" alt={buttonContent} />
-            <span className="ml-3">{buttonContent}</span>
+            <img src={imgURL} className="w-6 h-6 mr-3" alt={buttonContent} />
+            <span>{buttonContent}</span>
         </button>
     );
 }
-
