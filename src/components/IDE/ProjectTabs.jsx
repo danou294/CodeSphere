9615f62c1cc0ceb2d99af67a04db99dde51c342c
daniellevy <danou29442@gmail.com>
2024-03-18 11:58:43 +0100

    env

--- src/components/IDE/ProjectTabs.jsx
+++ src/components/IDE/ProjectTabs.jsx
@@ -1,15 +1,31 @@
-// Tabs.jsx
-import React, { useState } from 'react';
-import { useSelector } from "react-redux";
-import ButtonTab from "./ButtonTab.jsx";
-import CodeTab from "./CodeTab.jsx";
+import React, { useState, useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import ButtonTab from '../IDE/ButtonTab';
+import CodeTab from '../IDE/CodeTab';
+import PreviewButton from '../IDE/PreviewButton';
+import { useLocation } from 'react-router-dom';
 import Preview from "./Preview.jsx";
-import PreviewButton from "./PreviewButton.jsx";
 
-export default function Tabs() {
+export default function ProjectTabs() {
     const tabs = useSelector(state => state.tabs);
     const previewData = useSelector(state => state.preview);
-    const [tabIndex, setTabIndex] = useState(tabs[0]?.id || 0);
+    const [tabIndex, setTabIndex] = useState(0);
+    const location = useLocation();
+    const searchParams = new URLSearchParams(location.search);
+
+    useEffect(() => {
+        const tabIndexParam = searchParams.get('tabIndex');
+        if (tabIndexParam) {
+            console.log("tabIndex parameter:", tabIndexParam);
+            setTabIndex(parseInt(tabIndexParam));
+        } else {
+            console.log("tabIndex parameter not found in URL");
+        }
+    }, [location.search]);
+
+    useEffect(() => {
+        console.log("Current tabIndex:", tabIndex);
+    }, [tabIndex]);
 
     return (
         <div className="flex flex-grow">
--- src/components/ProjectCRUD/ProjectList.jsx
+++ src/components/ProjectCRUD/ProjectList.jsx
@@ -1,6 +1,7 @@
 // ProjectList.jsx
+
 import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { collection, getDocs, deleteDoc, doc } from 'firebase/firestore';
 import { useAuth } from '../contexts/AuthContext';
 import { firestore } from '../../firebaseConfig';
@@ -10,6 +11,10 @@ import PreviewPopup from './PreviewPopup';
 const ProjectList = () => {
     const [projects, setProjects] = useState([]);
     const { currentUser } = useAuth();
+    const [popupContent, setPopupContent] = useState(null);
+    const [popupPosition, setPopupPosition] = useState({ left: 0, top: 0 });
+    const [isPopupVisible, setPopupVisibility] = useState(false);
+    const navigate = useNavigate();
 
     useEffect(() => {
         const fetchUserProjects = async () => {
@@ -24,55 +29,73 @@ const ProjectList = () => {
         fetchUserProjects();
     }, [currentUser]);
 
+    const handleDelete = async (projectId) => {
+        try {
+            await deleteDoc(doc(firestore, 'projects', projectId));
+            setProjects(projects.filter(project => project.id !== projectId));
+        } catch (error) {
+            console.error('Error deleting project: ', error);
+        }
+    };
+
+    const handleProjectHover = (event, project) => {
+        if (!isPopupVisible) {
+            setPopupContent(project);
+            setPopupPosition({
+                left: event.target.offsetLeft,
+                top: event.target.offsetTop + event.target.offsetHeight,
+            });
+            setPopupVisibility(true);
+        }
+    };
+
+    const handleProjectClick = (projectId, tabIndex) => {
+        console.log('Redirecting to project:', projectId, 'with tabIndex:', tabIndex);
+        navigate(`/project/${projectId}?tabIndex=${tabIndex}`, { replace: true });
+    };
+
+    const handleMouseLeave = () => {
+        setPopupVisibility(false);
+    };
+
     return (
         <div className="container mx-auto">
             <h1 className="text-3xl font-bold mt-5 mb-8">Liste des projets</h1>
             <div className="grid grid-cols-3 gap-4">
                 {projects.map(project => (
-                    <ProjectItem key={project.id} project={project} />
+                    <div
+                        key={project.id}
+                        className="relative"
+                        onMouseEnter={(event) => handleProjectHover(event, project)}
+                        onMouseLeave={handleMouseLeave}
+                        onClick={() => handleProjectClick(project.id, project.tabIndex)}
+                    >
+                        <div className="bg-gray-200 p-6 rounded cursor-pointer hover:shadow-lg transition duration-300 ease-in-out transform hover:-translate-y-1">
+                            <h2 className="text-xl font-semibold mb-3 text-blue-500">{project.title}</h2>
+                            <p className="text-gray-700">{project.description}</p>
+                            <div className="flex justify-between mt-4">
+                                <Link to={`/update-project/${project.id}`}>
+                                    <button className="bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-2 px-4 rounded">
+                                        Modifier
+                                    </button>
+                                </Link>
+                                <DeleteProject projectId={project.id} onDelete={handleDelete} />
+                            </div>
+                        </div>
+                    </div>
                 ))}
             </div>
+            {isPopupVisible && (
+                <PreviewPopup
+                    project={popupContent}
+                    position={popupPosition}
+                />
+            )}
             <Link to="/create-project">
                 <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
                     <span className="text-lg">+</span> Ajouter un projet
                 </button>
             </Link>
-        </div>
-    );
-};
-
-const ProjectItem = ({ project }) => {
-    const [isMouseOverProject, setIsMouseOverProject] = useState(false);
-
-    const handleMouseEnter = () => {
-        setIsMouseOverProject(true);
-    };
-
-    const handleMouseLeave = () => {
-        setIsMouseOverProject(false);
-    };
-
-    return (
-        <div
-            className="relative"
-            onMouseEnter={handleMouseEnter}
-            onMouseLeave={handleMouseLeave}
-        >
-            <div className="bg-gray-200 p-6 rounded cursor-pointer hover:shadow-lg transition duration-300 ease-in-out transform hover:-translate-y-1">
-                <h2 className="text-xl font-semibold mb-3 text-blue-500">{project.title}</h2>
-                <p className="text-gray-700">{project.description}</p>
-                <div className="flex justify-between mt-4">
-                    <Link to={`/update-project/${project.id}`}>
-                        <button className="bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-2 px-4 rounded">
-                            Modifier
-                        </button>
-                    </Link>
-                    <DeleteProject projectId={project.id} />
-                </div>
-            </div>
-            {isMouseOverProject && (
-                <PreviewPopup project={project} />
-            )}
         </div>
     );
 };
